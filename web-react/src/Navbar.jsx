ab7f7591a9b5279f8bf8a6678332c8b9ad1f3958
omar <hmoqbil3@gmail.com>
2024-09-01 13:02:32 +0300

    react

--- web-react/src/Navbar.jsx
+++ web-react/src/Navbar.jsx
@@ -3,21 +3,26 @@ import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
 import { useEffect, useContext } from "react";
-import CartContext from "./Cards";
+import CartContext from "./CartContext";
 
-// useState initializes isOpen to false and provides a function setIsOpen to update it
-// toggleMenu is a function that toggles the value of isOpen
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const {items}= useContext(CartContext)
+  
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+  const totalQuantity = items.reduce((total, item) => total + item.quantity, 0);
+  useEffect(()=>{
+    
+  },[])
   return (
     <nav className="bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center p-3">
-      <div className="lg:hidden">
-              <Link to="/your-cart" onClick={toggleMenu}>
+      <div className="lg:hidden ">
+              <Link to="/your-cart" className="relative">
+                <span className="absolute bg-orange-400 text-black py-1 px-3 rounded-full right-[-30px] top-[-10px]">{totalQuantity > 10 ? "+10" :totalQuantity }
+                </span>
                 <CiShoppingCart className="text-4xl" />
               </Link>
             </div>
@@ -29,8 +34,8 @@ const Navbar = () => {
           {isOpen ? <FaTimes /> : <FaBars />}
           
         </button>
-        <div className={`absolute top-14 right-0 w-full bg-white shadow-md lg:static lg:w-auto lg:shadow-none ${isOpen ? "block" : "hidden"} lg:block`}>
-          <ul className="flex flex-col lg:flex-row lg:items-center lg:space-x-6">
+        <div className={`absolute top-14 w-full bg-white shadow-md lg:static mid lg:w-auto z-10 lg:shadow-none ${isOpen ? "block" : "hidden"} lg:block`}>
+          <ul className="flex flex-col lg:flex-row lg:items-end md:space-x-6">
             <li>
               <Link to="/about-us" className="block py-2 px-4 lg:py-0" onClick={toggleMenu}>About Us</Link>
             </li>
@@ -59,3 +64,6 @@ const Navbar = () => {
   );
 };
 export default Navbar;
+
+// useState initializes isOpen to false and provides a function setIsOpen to update it
+// toggleMenu is a function that toggles the value of isOpen
